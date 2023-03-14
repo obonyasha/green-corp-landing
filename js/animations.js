a65475a4e874cd103b0cf4a03d88bffdfde2174d
Tatiana Stepanova <obonyasha@gmail.com>
2023-03-14 14:52:08 +0500

    add updateScroll

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,7 @@
 const INCREASE_NUMBER_ANIMATION_SPEED = 50;
+let animationInited = false;
+
+//Счетчик клиентов
 
 function increaseNumberAnimationStep(i, element, endNumber) {
     if (i <= endNumber) {
@@ -18,7 +21,8 @@ function initIncreaseNumberAnimation() {
     let element = document.querySelector(".features__clients-count");
     increaseNumberAnimationStep(0, element, 5000)
 }
-initIncreaseNumberAnimation();
+
+//Добавление поля "Введите ваш вариант"
 
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
     if (event.target.value === 'other') {
@@ -37,8 +41,28 @@ document.querySelector('#budget').addEventListener('change', function handleSele
     const otherInput = document.querySelector('.form__other-input');
     if (event.target.value !== 'other' && Boolean(otherInput)) {
         // Удаляем ранее добавленное текстовое поле, если оно есть в DOM
-
         document.querySelector('.form form').removeChild(otherInput);
     }
 
 });
+
+//Изменение свойств header и счетчика клиентов при использовании скролл
+
+function updateScroll() {
+    if (window.scrollY > 0) {
+        document.querySelector("header").classList.add("header__scrolled");
+    } else {
+        document.querySelector("header").classList.remove("header__scrolled");
+    }
+    // Запуск анимации увеличения числа
+
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+    let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+
+    if(windowBottomPosition>=countElementPosition && !animationInited){
+        animationInited=true;
+        initIncreaseNumberAnimation();
+    }
+}
+
+window.addEventListener("scroll", updateScroll)
